85ef148c98c05ed020628723201782432314aae5
xxvampxx <simong01642@hotmail.co.uk>
2024-04-25 16:34:15 +0100

    initial commit
    initial commit of scrimba unit converter. however, i am not happy with my current code as i repeat myself a lot and i will find a better solution in time.

---
+++ app.js
@@ -0,0 +1,48 @@
+const inputEl = document.getElementById("input-el");
+const convertBtn = document.getElementById("convert-btn");
+
+let lengthContent = document.getElementById("length-content");
+let volumeContent = document.getElementById("volume-content");
+let massContent = document.getElementById("mass-content");
+
+convertBtn.addEventListener("click", function () {
+    //get inputEl value
+    const inputValue = inputEl.value;
+    //convert length function
+    function convertLengthToFeet() {
+        outputValue = inputValue * 3.281;
+        return outputValue;
+    }
+    function convertLengthToMeters() {
+        outputValue = inputValue / 3.281;
+        return outputValue;
+    }
+    //change p tag content
+    lengthContent.innerText = `${inputEl.value} meters = ${convertLengthToFeet().toFixed(3)} feet | ${inputEl.value} feet = ${convertLengthToMeters().toFixed(3)} meters `;
+
+    //convert volume function
+    function convertVolumeToGallons() {
+        outputValue = inputValue * 0.264;
+        return outputValue;
+    }
+    function convertVolumeToLiters() {
+        outputValue = inputValue / 0.264;
+        return outputValue;
+    }
+    //change p tag content
+    volumeContent.innerText = `${inputEl.value} liters = ${convertVolumeToGallons().toFixed(3)} gallons | ${inputEl.value} gallons = ${convertVolumeToLiters().toFixed(3)} liters `;
+
+    //convert mass function
+    function convertMassToPounds() {
+        outputValue = inputValue * 2.204;
+        return outputValue;
+    }
+    function convertMassToKilos() {
+        outputValue = inputValue / 2.204;
+        return outputValue;
+    }
+    //change p tag content
+    massContent.innerText = `${inputEl.value} pounds = ${convertMassToPounds().toFixed(3)} | ${inputEl.value} pounds = ${convertMassToKilos().toFixed(3)}`;
+
+})
+
